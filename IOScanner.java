56572805b3286ed2d31ffafdb4d8e5d9495e8fa5
Andrew Demchenko <demch.andrew@gmail.com>
2022-09-06 11:58:09 +0200

    Created main java file and a custom scanner file

---
+++ IOScanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+                                    // Custom input scanner class. 
+                                    // Do not create main function here
+
+public class IOScanner {
+                                    // custom input functions here ->
+                                    
+                                    // some test code for later 
+    // public int[] readFiveNums() {
+    //     int[] inputNums = new int[5];
+    //     Scanner in = new Scanner(System.in);
+    //     for(int i = 0; i < 5; ++i) {
+    //         inputNums[i] = in.nextInt();
+    //     }
+    //     in.close();
+    //     return inputNums;
+    // }             
+}
---
+++ ScoresSystem.java
@@ -0,0 +1,13 @@
+                                                // compile with javac *.java
+                                                // run with java ScoresSystem.java
+
+public class ScoresSystem {
+                                                // functions here ->
+
+    public static void main(String[] args) {
+        
+                                                // some test code for later
+        // IOScanner in = new IOScanner();
+        // System.out.println(in.readFiveNums()[2]);
+    }
+}
